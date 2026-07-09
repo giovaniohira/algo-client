6c5a16d9992e072e18bd4919287c3fd7cdb48103
Giovani Ohira <giovanirossi863@gmail.com>
2026-07-09 19:10:03 -0300

    chore: update lockfile, tsconfig and renderer entry

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,12 +2,8 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import * as monaco from 'monaco-editor'
 import App from './App'
 import './styles.css'
-import { setupMonaco } from './lib/monaco-setup'
-
-setupMonaco(monaco)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
